de9bcbc7814ca951a7432b53a3bb233a53a8ba16
pasichDev <apasichnik9@gmail.com>
2025-09-03 20:24:13 +0300

    Revert "Fixed animation when changing tag visibility"
    This reverts commit 04f81984657b1ab568d1c5b56003c42c0eea757b.

--- app/src/main/java/com/pasich/mynotes/utils/adapters/notes/NoteAdapter.java
+++ app/src/main/java/com/pasich/mynotes/utils/adapters/notes/NoteAdapter.java
@@ -62,11 +62,10 @@ public class NoteAdapter<VM extends ViewDataBinding> extends GenericAdapter<Note
     public void sortList(String arg) {
         ArrayList<Note> newList = new ArrayList<>(getCurrentList());
         newList.sort(new NoteComparator().getComparator(arg));
-        submitList(newList);
         submitList(newList, () -> {
             if (shouldScrollToTop && recyclerView != null && !newList.isEmpty()) {
                 recyclerView.post(() -> {
-                    androidx.recyclerview.widget.StaggeredGridLayoutManager layoutManager =
+                    androidx.recyclerview.widget.StaggeredGridLayoutManager layoutManager = 
                         (androidx.recyclerview.widget.StaggeredGridLayoutManager) recyclerView.getLayoutManager();
                     if (layoutManager != null) {
                         layoutManager.scrollToPositionWithOffset(0, 0);
