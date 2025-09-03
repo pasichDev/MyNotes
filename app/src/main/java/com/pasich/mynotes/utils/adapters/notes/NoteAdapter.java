6b08ae57abe7d4d00c0f0473900721b6f6670a87
pasichDev <apasichnik9@gmail.com>
2025-09-03 20:25:31 +0300

    Fixed

--- app/src/main/java/com/pasich/mynotes/utils/adapters/notes/NoteAdapter.java
+++ app/src/main/java/com/pasich/mynotes/utils/adapters/notes/NoteAdapter.java
@@ -144,7 +144,6 @@ public class NoteAdapter<VM extends ViewDataBinding> extends GenericAdapter<Note
         }
 
     }
-    
     public void clearList() {
         submitList(new ArrayList<>());
     }
