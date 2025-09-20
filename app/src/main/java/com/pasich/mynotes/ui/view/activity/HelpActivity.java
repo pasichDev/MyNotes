0d39be45d27fe18f68ef31c5248b5acd54a88965
pasichDev <apasichnik9@gmail.com>
2025-09-20 21:15:18 +0300

    #77

--- app/src/main/java/com/pasich/mynotes/ui/view/activity/HelpActivity.java
+++ app/src/main/java/com/pasich/mynotes/ui/view/activity/HelpActivity.java
@@ -29,7 +29,7 @@ public class HelpActivity extends BaseActivity {
     @Named("NotesItemSpaceDecoration")
     public SpacesItemDecoration itemDecoration;
 
-    private final String actualVersionHelp = "2.1.33";
+    private final String actualVersionHelp = "2.1.35";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
