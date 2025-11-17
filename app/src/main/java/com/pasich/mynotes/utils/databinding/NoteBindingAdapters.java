f1e1b1d02f6be2ea600f50c6489f5f3d1250122f
pasichDev <apasichnik9@gmail.com>
2025-11-17 10:43:23 +0200

    #117 Displaying the number of pinned files item_note.xml

--- app/src/main/java/com/pasich/mynotes/utils/databinding/NoteBindingAdapters.java
+++ app/src/main/java/com/pasich/mynotes/utils/databinding/NoteBindingAdapters.java
@@ -1,5 +1,6 @@
 package com.pasich.mynotes.utils.databinding;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -8,6 +9,7 @@ import androidx.databinding.BindingAdapter;
 import com.pasich.mynotes.R;
 import com.pasich.mynotes.data.model.Note;
 import com.pasich.mynotes.utils.FormattedDataUtil;
+import com.pasich.mynotes.utils.noteEditor.models.ParsedNote;
 
 public class NoteBindingAdapters {
 
@@ -16,6 +18,23 @@ public class NoteBindingAdapters {
         if (note != null && note.getDate() > 0) {
             textView.setText(
                     textView.getContext().getString(R.string.lastDateEditNote,  FormattedDataUtil.lastDayEditNote(note.getDate()))
+            );
+            textView.setVisibility(View.VISIBLE);
+        } else {
+            textView.setText("");
+            textView.setVisibility(View.GONE);
+        }
+    }
+
+    @BindingAdapter("noteMediaCount")
+    public static void setNoteMediaCount(TextView textView, Note note) {
+        int countMedia = ParsedNote.parseAttachmentsJson(note.getAttachments()).size();
+        Log.e("s", note.getAttachments());
+        Log.e("s", String.valueOf(countMedia));
+
+        if (countMedia > 0) {
+            textView.setText(
+                    textView.getContext().getString(R.string.countMediaNote, countMedia)
             );
             textView.setVisibility(View.VISIBLE);
         } else {
--- app/src/main/java/com/pasich/mynotes/utils/noteEditor/models/ParsedNote.java
+++ app/src/main/java/com/pasich/mynotes/utils/noteEditor/models/ParsedNote.java
@@ -1,9 +1,31 @@
 package com.pasich.mynotes.utils.noteEditor.models;
 
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ParsedNote {
     public String plainText = "";
     public List<EditorAttachment> attachments = new ArrayList<>();
+
+    public static List<EditorAttachment> parseAttachmentsJson(String json) {
+        if (json == null || json.isEmpty()) {
+            return new ArrayList<>();
+        }
+
+        try {
+            Gson gson = new Gson();
+            Type type = new TypeToken<List<EditorAttachment>>() {}.getType();
+            return gson.fromJson(json, type);
+        } catch (Exception e) {
+            Log.e("jsonToModel", "Failed to parse attachments JSON", e);
+            return new ArrayList<>();
+        }
+    }
+
 }
