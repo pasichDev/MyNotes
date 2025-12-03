6e877e2b9eca80930919a8c4cf1e9e18891cb228
pasichDev <apasichnik9@gmail.com>
2025-12-03 21:11:38 +0200

    #130 fix libsActivity open

--- app/src/main/java/com/pasich/mynotes/ui/view/activity/LibsActivity.java
+++ app/src/main/java/com/pasich/mynotes/ui/view/activity/LibsActivity.java
@@ -25,6 +25,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
 public class LibsActivity extends BaseActivity {
 
     protected ActivityLibsBinding binding;
