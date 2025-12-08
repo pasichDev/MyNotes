fdd8537135c4fb0697e173a6017ca05c9f6d95a8
pasichDev <apasichnik9@gmail.com>
2025-12-08 20:40:08 +0200

    #144 clean check

--- app/src/main/java/com/pasich/mynotes/data/model/Note.java
+++ app/src/main/java/com/pasich/mynotes/data/model/Note.java
@@ -1,7 +1,6 @@
 package com.pasich.mynotes.data.model;
 
 import androidx.room.Entity;
-import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 
 import com.google.gson.JsonArray;
@@ -42,15 +41,12 @@ public class Note {
     // NEW: instead of TrashNote table
     @SerializedName("i")
     private boolean isTrash = false;
-    @Ignore
-    private boolean Checked;
 
     public Note create(String title, String value, long date, String tag) {
         this.title = title;
         this.tag = tag;
         this.value = value;
         this.date = date;
-        this.Checked = false;
         return this;
     }
 
@@ -59,7 +55,6 @@ public class Note {
         this.tag = "";
         this.value = value;
         this.date = date;
-        this.Checked = false;
         return this;
     }
 
@@ -113,14 +108,6 @@ public class Note {
         return value.length() > 400 ? value.substring(0, 400) : value;
     }
 
-
-    public boolean getChecked() {
-        return this.Checked;
-    }
-
-    public void setChecked(boolean arg) {
-        this.Checked = arg;
-    }
 
     public long getDate() {
         return this.date;
@@ -179,7 +166,6 @@ public class Note {
         this.valueJson = other.valueJson;
         this.hasRichContent = other.hasRichContent;
         this.attachments = other.attachments;
-        this.Checked = other.Checked;
     }
 
     public Note duplicate() {
