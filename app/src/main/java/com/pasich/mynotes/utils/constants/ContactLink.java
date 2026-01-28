b2c87b9c9724597c5fb7b2fd1f364d5fa67c6cdf
pasichDev <apasichnik9@gmail.com>
2026-01-28 15:03:04 +0200

    update links pp && home site

--- app/src/main/java/com/pasich/mynotes/utils/constants/ContactLink.java
+++ app/src/main/java/com/pasich/mynotes/utils/constants/ContactLink.java
@@ -1,9 +1,9 @@
 package com.pasich.mynotes.utils.constants;
 
 public class ContactLink {
-    public static final String LINK_PRIVACY_POLICY = "https://mynotes.pasichdev.xyz/privacy-policy.html";
+    public static final String LINK_PRIVACY_POLICY = "https://pasichdev.xyz/apps/mynotes/privacy-policy";
     public static final String LINK_GITHUB_REPO = "https://github.com/pasichDev/MyNotes";
-    public static final String LINK_APP_SITE = "https://mynotes.pasichdev.xyz/index.html";
+    public static final String LINK_APP_SITE = "https://pasichdev.xyz/apps/mynotes";
     public static final String SEND_FEEDBACK = "https://tally.so/r/3xbVay";
     public static final String SEND_FEEDBACK_EDITOR = "https://tally.so/r/w87L1Y";
 }
